b27ca318ed580ec4c0d55168c0bce4bd24037f69
Ericqwqw1212 <supersneakyfox@gmail.com>
2019-05-08 09:58:56 -0600

    Wednessday Work

--- Assets/Scripts/Environment/PuzzlePiece.cs
+++ Assets/Scripts/Environment/PuzzlePiece.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SphereCollider))]
 public class PuzzlePiece : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject[] m_gameObjects;
+    [SerializeField] int m_currentIndex = 0;
+    [SerializeField] GameObject m_currentObject;
+
+    [SerializeField] int m_nodeField;
+    [SerializeField] int m_nodeID = 0;
+
     void Start()
     {
-        
+        if (m_currentIndex < 0 || m_currentIndex >= m_gameObjects.Length) m_currentIndex = 0;
+        m_currentObject = m_gameObjects[m_currentIndex];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Transform objectHit = hit.transform;
+
+                if (objectHit.position == this.transform.position)
+                {
+                    m_currentIndex++;
+                    if (m_currentIndex >= m_gameObjects.Length) m_currentIndex = 0;
+
+                    m_currentObject.GetComponent<MeshRenderer>() = m_gameObjects[m_currentIndex].GetComponent<MeshRenderer>();
+                }
+            }
+        }
     }
 }
