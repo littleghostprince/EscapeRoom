3f852d95e60e99b5260f49538e0ecb42a44b6f7d
Spencer <southam.spencer.d@gmail.com>
2019-05-08 09:59:24 -0600

    Barebones movement is Done

---
+++ Assets/Scripts/Movement/NodeWalker.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeWalker : MonoBehaviour
+{
+    public List<KeyCode> forwardKeys;
+    public List<KeyCode> leftKeys;
+    public List<KeyCode> downKeys;
+    public List<KeyCode> rightKeys;
+
+    public Node current = null;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (KeyCode key in forwardKeys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                if (current.m_nodes[0] != null)
+                {
+                    transform.position = current.m_nodes[0].transform.position;
+                    current = current.m_nodes[0];
+                }
+            }
+        }
+        foreach (KeyCode key in leftKeys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                if (current.m_nodes[1] != null)
+                {
+                    transform.position = current.m_nodes[1].transform.position;
+                    current = current.m_nodes[1];
+                }
+            }
+        }
+        foreach (KeyCode key in downKeys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                if (current.m_nodes[2] != null)
+                {
+                    transform.position = current.m_nodes[2].transform.position;
+                    current = current.m_nodes[2];
+                }
+            }
+        }
+        foreach (KeyCode key in rightKeys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                if (current.m_nodes[3] != null)
+                {
+                    transform.position = current.m_nodes[3].transform.position;
+                    current = current.m_nodes[3];
+                }
+            }
+        }
+    }
+}
