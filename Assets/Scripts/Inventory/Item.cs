54b933761635d4b0804ff7a8de915fd74724ec8d
littleghostprince <ashleysmejia@gmail.com>
2019-05-16 08:57:15 -0600

    5/16
    uh

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -6,10 +6,12 @@ public class Item : MonoBehaviour
 {
     [SerializeField] public GameObject m_item = null;
     [SerializeField] public Sprite itemIcon = null;
+
     public string m_name;
 
     public bool isCombinedItem = false; //is this an item that can only be made by combining items?
-   
+    public bool isUse = false; 
+
     //if so fill out this information! 
     public string needed_Item1;
     public string needed_Item2;
