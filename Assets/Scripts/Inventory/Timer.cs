e4d4bc1c0a1ee26ed483a46588f6a625170d6f57
Spencer <southam.spencer.d@gmail.com>
2019-05-21 09:02:55 -0600

    Timer 90% done

---
+++ Assets/Scripts/Inventory/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI uiText = null;
+
+    public float timePast { get { return timer; } }
+    private float timer = 0.0f;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        uiText.text = timer.ToString("F");
+    }
+}
