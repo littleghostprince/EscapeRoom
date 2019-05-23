bce639482157179ef10b8665030838f6ac83547e
Spencer <southam.spencer.d@gmail.com>
2019-05-23 08:59:19 -0600

    Added ScoreEntry

---
+++ Assets/Scripts/Score/ScoreEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ScoreEntry : IComparable
+{
+    public TimeSpan m_time = TimeSpan.Zero;
+    public string m_name = "";
+    public string m_ending = "";
+
+    public int CompareTo(object obj)
+    {
+        return m_time.CompareTo(obj);
+    }
+}
