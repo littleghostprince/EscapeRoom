12a6d2ee38831b3e21a16bf5e407eb2dec7d0bf5
littleghostprince <ashleysmejia@gmail.com>
2019-05-23 14:49:13 -0600

    5/23
    HAJSAKSJF

---
+++ Assets/Scripts/Menu/Buttons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    [SerializeField] GameObject m_scorePanel = null;
+
+    public void ShowPanel()
+    {
+        if (m_scorePanel.activeSelf == false)
+        {
+            m_scorePanel.SetActive(true);
+        }
+        else m_scorePanel.SetActive(false);
+    }
+    public void LoadMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
