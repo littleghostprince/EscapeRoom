db1cfc87cc93a81abe754f27b2990ff23e78592c
littleghostprince <ashleysmejia@gmail.com>
2019-05-24 08:27:24 -0600

    5/24
    static stuff

---
+++ Assets/Scripts/Menu/Audio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    [SerializeField] AudioSource m_mainMusic = null;
+    [SerializeField] AudioSource m_static = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_mainMusic.isPlaying)
+        {
+            m_mainMusic.Pause();
+            m_static.Play();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(m_mainMusic.isPlaying == false)
+        {
+            m_mainMusic.UnPause();
+            m_static.Stop();
+        }
+    }
+}
