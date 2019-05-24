da107384974605ca6a07d8d191ba95a5977537c5
Spencer <southam.spencer.d@gmail.com>
2019-05-23 19:30:00 -0600

    fixed some names, working on Movement implementation

--- Assets/Scripts/Editor/NodeUIEditor.cs
+++ Assets/Scripts/Editor/NodeUIEditor.cs
@@ -6,13 +6,13 @@ using UnityEditor;
 [CustomEditor(typeof(Node))]
 public class NodeUIEditor : Editor
 {
-    public override void OnInspectorGUI()
-    {
-        Node node = (Node)target;
-        node.m_ID = EditorGUILayout.TextField("Node ID", node.m_ID);
-        node.m_nodes[0] = (Node)EditorGUILayout.ObjectField("Forward", node.m_nodes[0], typeof(Node), true);
-        node.m_nodes[1] = (Node)EditorGUILayout.ObjectField("Left", node.m_nodes[1], typeof(Node), true);
-        node.m_nodes[2] = (Node)EditorGUILayout.ObjectField("Back", node.m_nodes[2], typeof(Node), true);
-        node.m_nodes[3] = (Node)EditorGUILayout.ObjectField("Right", node.m_nodes[3], typeof(Node), true);
-    }
+    //public override void OnInspectorGUI()
+    //{
+    //    Node node = (Node)target;
+    //    node.m_ID = EditorGUILayout.TextField("Node ID", node.m_ID);
+    //    node.m_nodes[0] = (Node)EditorGUILayout.ObjectField("Forward", node.m_nodes[0], typeof(Node), true);
+    //    node.m_nodes[1] = (Node)EditorGUILayout.ObjectField("Left", node.m_nodes[1], typeof(Node), true);
+    //    node.m_nodes[2] = (Node)EditorGUILayout.ObjectField("Back", node.m_nodes[2], typeof(Node), true);
+    //    node.m_nodes[3] = (Node)EditorGUILayout.ObjectField("Right", node.m_nodes[3], typeof(Node), true);
+    //}
 }
--- Assets/Scripts/Movement/Node.cs
+++ Assets/Scripts/Movement/Node.cs
@@ -5,7 +5,5 @@ using UnityEngine;
 public class Node : MonoBehaviour
 {
     public string m_ID = "";
-    //public List<Node> m_nodes = new List<Node>();
-    //[NamedArrayAttribute(new string[] { "Forward", "Left", "Back", "Right" })]
     public Node[] m_nodes = new Node[4];
 }
